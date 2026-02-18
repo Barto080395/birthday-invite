4158bed5d06f7e82fbef2f7a5415c2976f320799
Barto080395 <bartolomeobraccio95@live.com>
2026-02-18 13:58:18 +0100

    fix menu

--- components/ui/BirthdayInvite/SettingsMenu.tsx
+++ components/ui/BirthdayInvite/SettingsMenu.tsx
@@ -114,7 +114,7 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 35,
     right: 10,
-    width: 160,
+    width: 162,
     backgroundColor: "#fff",
     borderRadius: 10,
     paddingVertical: 10,
